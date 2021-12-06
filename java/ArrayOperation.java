6234862b2c72265ff99114c9fb21f422742deccf
MF <ms.delo000@mail.ru>
2021-12-06 22:12:34 +0700

    dz2one3

---
+++ java/ArrayOperation.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+public class ArrayOperation {
+    public static List<String> fillTheArrayFromFile(List<String> wordArray, String fileName2) {
+        return wordArray;
+    }
+
+    public static void printUniqueWords(List<String> wordArray) {
+    }
+
+    public static List<String> fillTheArray(List<String> wordArray) {
+        return wordArray;
+    }
+}
---
+++ java/Phonebook.java
@@ -0,0 +1,7 @@
+public class Phonebook {
+    public void add(int i, String ivanov) {
+    }
+
+    public void get(String ivanov) {
+    }
+}
